0ba86562c291503c677ff114e4410ee496942878
stdutil <363408268@qq.com>
2019-04-10 15:41:12 +0800

    数据库备份后台任务

--- src/main/java/net/stackoverflow/blog/BlogApplication.java
+++ src/main/java/net/stackoverflow/blog/BlogApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCaching
 @EnableTransactionManagement
 @EnableScheduling
+@EnableAsync
 public class BlogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/net/stackoverflow/blog/task/BackupTask.java
@@ -0,0 +1,33 @@
+package net.stackoverflow.blog.task;
+
+import net.stackoverflow.blog.util.DBUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 数据库备份任务
+ *
+ * @author 凉衫薄
+ */
+@Component
+public class BackupTask {
+
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.host}")
+    private String host;
+    @Value("${spring.datasource.db}")
+    private String db;
+    @Value("${server.upload.path}")
+    private String path;
+
+    @Scheduled(initialDelay = 10000, fixedRate = 600000)
+    @Async
+    public void backup() {
+        DBUtils.backup(host, username, password, path, "blog.sql", db);
+    }
+}
