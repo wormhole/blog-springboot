dbae7a7330da1ca74eef81d71eba2677bba4f9be
stdutil <363408268@qq.com>
2019-04-12 16:22:04 +0800

    个人详情页接口

--- src/main/java/net/stackoverflow/blog/web/controller/admin/user/PersonalController.java
+++ src/main/java/net/stackoverflow/blog/web/controller/admin/user/PersonalController.java
@@ -1,4 +1,4 @@
-package net.stackoverflow.blog.web.controller.api.admin;
+package net.stackoverflow.blog.web.controller.admin.user;
 
 import net.stackoverflow.blog.common.BaseController;
 import net.stackoverflow.blog.common.BaseDTO;
@@ -14,7 +14,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.ConstraintViolation;
@@ -26,13 +31,12 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * 用户数据获取接口Controller
+ * 个人信息维护接口
  *
- * @author 凉衫薄
+ * @author 凉衫薄
  */
-@RestController
-@RequestMapping("/api/admin")
-public class UserController extends BaseController {
+@Controller
+public class PersonalController extends BaseController {
 
     @Autowired
     private UserService userService;
@@ -42,36 +46,47 @@ public class UserController extends BaseController {
     private ValidatorFactory validatorFactory;
 
     /**
-     * 用户信息更新接口
-     * /api/admin/user/update
-     * 方法 POST
+     * 个人信息维护页面跳转
      *
-     * @param type
-     * @param dto
-     * @param session
      * @return
      */
-    @RequestMapping(value = "/user/update", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/personal", method = RequestMethod.GET)
+    public ModelAndView personal() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/admin/user/personal");
+        return mv;
+    }
+
+    /**
+     * 用户信息更新接口
+     *
+     * @param type    更新类型：1、base基础数据，2、password密码
+     * @param dto     公共dto对象
+     * @param session 会话对象
+     * @return 返回Response对象
+     */
+    @RequestMapping(value = "/api/admin/user/update", method = RequestMethod.POST)
     public Response updateUser(@RequestParam("type") String type, @RequestBody BaseDTO dto, HttpSession session) {
         Response response = new Response();
 
-        List<UserDTO> dtos = (List<UserDTO>) (Object) getDTO("user", UserDTO.class, dto);
-        if (CollectionUtils.isEmpty(dtos)) {
+        //从公共dto中获取userDTO
+        List<UserDTO> userDTOs = (List<UserDTO>) (Object) getDTO("user", UserDTO.class, dto);
+        if (CollectionUtils.isEmpty(userDTOs)) {
             throw new BusinessException("找不到请求数据");
         }
-        UserDTO userDTO = dtos.get(0);
+        UserDTO userDTO = userDTOs.get(0);
         UserPO user = (UserPO) session.getAttribute("user");
 
         if (type.equals("base")) {
-
+            //校验数据
             Validator validator = validatorFactory.getValidator();
             Set<ConstraintViolation<UserDTO>> violations = validator.validate(userDTO, UserDTO.UpdateBaseGroup.class);
             Map<String, String> map = ValidationUtils.errorMap(violations);
-
             if (!CollectionUtils.isEmpty(map)) {
                 throw new BusinessException("字段格式出错", map);
             }
 
+            //校验邮箱是否重复
             if (!userDTO.getEmail().equals(user.getEmail()) && userService.selectByCondition(new HashMap<String, Object>() {{
                 put("email", userDTO.getEmail());
             }}).size() != 0) {
@@ -82,6 +97,7 @@ public class UserController extends BaseController {
             BeanUtils.copyProperties(userDTO, updateUser);
             updateUser.setId(user.getId());
 
+            //删除缓存
             if (!updateUser.getEmail().equals(user.getEmail())) {
                 Cache authenticationCache = redisCacheManager.getCache("authentication");
                 authenticationCache.evict("shiro:authentication:" + user.getEmail());
@@ -89,21 +105,22 @@ public class UserController extends BaseController {
                 authorizationCache.evict("shiro:authorization:" + user.getEmail());
             }
 
+            //更新用户基础信息
             UserPO newUser = userService.update(updateUser);
             session.setAttribute("user", newUser);
             response.setStatus(Response.SUCCESS);
             response.setMessage("基础信息修改成功");
 
         } else if (type.equals("password")) {
-
+            //校验数据
             Validator validator = validatorFactory.getValidator();
             Set<ConstraintViolation<UserDTO>> violations = validator.validate(userDTO, UserDTO.UpdatePasswordGroup.class);
             Map<String, String> map = ValidationUtils.errorMap(violations);
-
             if (!CollectionUtils.isEmpty(map)) {
                 throw new BusinessException("字段格式出错", map);
             }
 
+            //检查是否与旧密码匹配
             if (!user.getPassword().equals(PasswordUtils.encryptPassword(user.getSalt(), userDTO.getOldPassword()))) {
                 throw new BusinessException("旧密码不匹配");
             }
@@ -115,6 +132,7 @@ public class UserController extends BaseController {
             updateUser.setSalt(PasswordUtils.getSalt());
             updateUser.setPassword(PasswordUtils.encryptPassword(updateUser.getSalt(), updateUser.getPassword()));
 
+            //删除缓存
             Cache authenticationCache = redisCacheManager.getCache("authentication");
             authenticationCache.evict("shiro:authentication:" + user.getEmail());
             Cache authorizationCache = redisCacheManager.getCache("authorization");
