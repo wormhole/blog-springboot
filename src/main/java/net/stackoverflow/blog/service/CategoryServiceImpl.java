5e90dcc65a0f5b8b8f061586987cec6a28c646fc
stdutil <363408268@qq.com>
2019-04-15 15:32:06 +0800

    分类服务，缓存操作

--- src/main/java/net/stackoverflow/blog/service/CategoryServiceImpl.java
+++ src/main/java/net/stackoverflow/blog/service/CategoryServiceImpl.java
@@ -27,18 +27,36 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private ArticleDao articleDao;
 
+    /**
+     * 分页查询
+     *
+     * @param page 分页参数
+     * @return 返回结果集
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
     public List<CategoryPO> selectByPage(Page page) {
         return categoryDao.selectByPage(page);
     }
 
+    /**
+     * 条件查询
+     *
+     * @param searchMap 条件查询参数
+     * @return 返回结果集
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
     public List<CategoryPO> selectByCondition(Map<String, Object> searchMap) {
         return categoryDao.selectByCondition(searchMap);
     }
 
+    /**
+     * 根据id查询
+     *
+     * @param id 分类主键
+     * @return 返回查询结果
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
     public CategoryPO selectById(String id) {
@@ -52,42 +70,60 @@ public class CategoryServiceImpl implements CategoryService {
             }
             return categoryPO;
         }
-
     }
 
+    /**
+     * 新增分类
+     *
+     * @param categoryPO 新增的分类PO
+     * @return 新增后的分类PO
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public CategoryPO insert(CategoryPO category) {
-        categoryDao.insert(category);
-        RedisCacheUtils.set("category:" + category.getId(), category);
-        return category;
+    public CategoryPO insert(CategoryPO categoryPO) {
+        categoryDao.insert(categoryPO);
+        RedisCacheUtils.set("category:" + categoryPO.getId(), categoryPO);
+        return categoryPO;
     }
 
+    /**
+     * 批量新增
+     *
+     * @param categoryPOs 批量新增的文章列表
+     * @return 返回批量新增的记录数
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public int batchInsert(List<CategoryPO> categorys) {
-        return categoryDao.batchInsert(categorys);
+    public int batchInsert(List<CategoryPO> categoryPOs) {
+        return categoryDao.batchInsert(categoryPOs);
     }
 
+    /**
+     * 根据id删除分类
+     *
+     * @param id 被删除的分类主键
+     * @return 返回被删除的分类PO
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
     public CategoryPO deleteById(String id) {
         CategoryPO category = categoryDao.selectById(id);
 
-        CategoryPO unCategory = categoryDao.selectByCondition(new HashMap<String, Object>() {{
+        //将所有该分类的文章设成未分类
+        CategoryPO unCategoryPO = categoryDao.selectByCondition(new HashMap<String, Object>() {{
             put("code", "uncategory");
         }}).get(0);
-        List<ArticlePO> articleList = articleDao.selectByCondition(new HashMap<String, Object>() {{
+        List<ArticlePO> articlePOs = articleDao.selectByCondition(new HashMap<String, Object>() {{
             put("categoryId", category.getId());
         }});
 
-        if (articleList.size() != 0) {
-            for (ArticlePO article : articleList) {
-                article.setCategoryId(unCategory.getId());
-                RedisCacheUtils.set("article:" + article.getId(), article);
-                RedisCacheUtils.set("article:" + article.getUrl(), article);
+        if (articlePOs != null && articlePOs.size() != 0) {
+            for (ArticlePO articlePO : articlePOs) {
+                articlePO.setCategoryId(unCategoryPO.getId());
+                RedisCacheUtils.set("article:" + articlePO.getId(), articlePO);
+                RedisCacheUtils.set("article:" + articlePO.getUrl(), articlePO);
             }
-            articleDao.batchUpdate(articleList);
+            articleDao.batchUpdate(articlePOs);
         }
 
         categoryDao.deleteById(id);
@@ -95,44 +131,63 @@ public class CategoryServiceImpl implements CategoryService {
         return category;
     }
 
+    /**
+     * 根据id批量删除分类
+     *
+     * @param ids 被删除的分类id列表
+     * @return 返回被删除的记录数
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
     public int batchDeleteById(List<String> ids) {
         for (String id : ids) {
-            CategoryPO category = categoryDao.selectById(id);
+            CategoryPO categoryPO = categoryDao.selectById(id);
 
-            CategoryPO unCategory = categoryDao.selectByCondition(new HashMap<String, Object>() {{
+            //将该分类下的文章都设成未分类
+            CategoryPO unCategoryPO = categoryDao.selectByCondition(new HashMap<String, Object>() {{
                 put("code", "uncategory");
             }}).get(0);
-            List<ArticlePO> articleList = articleDao.selectByCondition(new HashMap<String, Object>() {{
-                put("categoryId", category.getId());
+            List<ArticlePO> articlePOs = articleDao.selectByCondition(new HashMap<String, Object>() {{
+                put("categoryId", categoryPO.getId());
             }});
-            if (articleList.size() != 0) {
-                for (ArticlePO article : articleList) {
-                    article.setCategoryId(unCategory.getId());
-                    RedisCacheUtils.set("article:" + article.getId(), article);
-                    RedisCacheUtils.set("article:" + article.getUrl(), article);
+            if (articlePOs != null && articlePOs.size() != 0) {
+                for (ArticlePO articlePO : articlePOs) {
+                    articlePO.setCategoryId(unCategoryPO.getId());
+                    RedisCacheUtils.set("article:" + articlePO.getId(), articlePO);
+                    RedisCacheUtils.set("article:" + articlePO.getUrl(), articlePO);
                 }
-                articleDao.batchUpdate(articleList);
+                articleDao.batchUpdate(articlePOs);
             }
             RedisCacheUtils.del("category:" + id);
         }
         return categoryDao.batchDeleteById(ids);
     }
 
+    /**
+     * 更新分类
+     *
+     * @param categoryPO 被更新的分类PO类
+     * @return 返回更新后的分类PO
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public CategoryPO update(CategoryPO category) {
-        categoryDao.update(category);
-        RedisCacheUtils.set("category:" + category.getId(), category);
-        return category;
+    public CategoryPO update(CategoryPO categoryPO) {
+        categoryDao.update(categoryPO);
+        RedisCacheUtils.set("category:" + categoryPO.getId(), categoryPO);
+        return categoryPO;
     }
 
+    /**
+     * 批量更新
+     *
+     * @param categoryPOs 批量更新的分类列表
+     * @return 返回被更新的记录数
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public int batchUpdate(List<CategoryPO> categorys) {
-        int result = categoryDao.batchUpdate(categorys);
-        for (CategoryPO categoryPO : categorys) {
+    public int batchUpdate(List<CategoryPO> categoryPOs) {
+        int result = categoryDao.batchUpdate(categoryPOs);
+        for (CategoryPO categoryPO : categoryPOs) {
             RedisCacheUtils.set("category:" + categoryPO.getId(), categoryPO);
         }
         return result;
--- src/test/java/net/stackoverflow/blog/BlogApplicationTests.java
+++ src/test/java/net/stackoverflow/blog/BlogApplicationTests.java
@@ -1,16 +1,36 @@
 package net.stackoverflow.blog;
 
+import net.stackoverflow.blog.pojo.po.MenuPO;
+import net.stackoverflow.blog.service.MenuService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogApplicationTests {
 
+    @Autowired
+    private MenuService service;
+
     @Test
     public void contextLoads() {
+
+        MenuPO menu = new MenuPO();
+        menu.setName("主页");
+        menu.setUrl("/");
+        menu.setDeleteAble(1);
+        menu.setDate(new Date());
+        List<MenuPO> list = new ArrayList<>();
+        list.add(menu);
+        service.batchInsert(list);
+        System.out.println();
     }
 
 }
